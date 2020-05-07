52442fd0753bf2c2d9640640714a3d9184601c80
zhaoxiangchun <zhaoxiangchun@yunion.cn>
2020-05-07 13:18:29 +0800

    report-server 监控间隔调整
    通过正则表达式匹配schedule的cron表达式中的调用周期，从而指定cronjob中的interval

--- pkg/apis/onecloud/v1alpha1/types.go
+++ pkg/apis/onecloud/v1alpha1/types.go
@@ -270,7 +270,6 @@ type CronJobSpec struct {
 	ContainerSpec
 	Disable      bool                `json:"disable"`
 	Schedule     string              `json:"schedule"`
-	Interval string `json:"interval"`
 	Affinity     *corev1.Affinity    `json:"affinity,omitempty"`
 	NodeSelector map[string]string   `json:"nodeSelector,omitempty"`
 	Tolerations  []corev1.Toleration `json:"tolerations,omitempty"`
