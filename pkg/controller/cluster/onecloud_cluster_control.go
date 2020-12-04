c03365e58a25fd1191e3872ed7957d06b3fa760d
zhaoxiangchun <1422928955@qq.com>
2020-12-04 16:22:36 +0800

    chore(monitor): add new service: suggestion
    1. 添加新component: suggestion
    2. suggestion 中getDBConfig 使用 monitor的连接信息

--- pkg/controller/cluster/onecloud_cluster_control.go
+++ pkg/controller/cluster/onecloud_cluster_control.go
@@ -132,6 +132,7 @@ func (occ *defaultClusterControl) updateOnecloudCluster(oc *v1alpha1.OnecloudClu
 		components.Telegraf(),
 		components.CloudId(),
 		components.Cloudmon(),
+		components.Suggestion(),
 	}
 	var grp errgroup.Group
 	for _, component := range dependComponents {
