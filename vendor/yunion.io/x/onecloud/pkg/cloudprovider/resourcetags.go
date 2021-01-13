aa530864c576c8482d86328cbe24651d38cba8bb
Qiu Jian <qiujian@yunionyun.com>
2021-01-13 17:52:59 +0800

    fix: update vendor

--- vendor/yunion.io/x/onecloud/pkg/cloudprovider/resourcetags.go
+++ vendor/yunion.io/x/onecloud/pkg/cloudprovider/resourcetags.go
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package multicloud
+package cloudprovider
 
-type SEipBase struct {
-	SResourceBase
-	SBillingBase
+type TagsUpdateInfo struct {
+	OldTags map[string]string
+	NewTags map[string]string
 }
