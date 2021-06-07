b5c27915a9289901edc3ab133cb627e29fdab219
zhaoxiangchun <1422928955@qq.com>
2021-06-07 15:18:13 +0800

    fix: update raid-plugin image tag
    1. 修改 raid-plugin image tag
    2. 修改 raid influxdb query time  from

--- pkg/apis/onecloud/v1alpha1/defaults.go
+++ pkg/apis/onecloud/v1alpha1/defaults.go
@@ -57,7 +57,7 @@ const (
 	DefaultTelegrafInitImageName = "telegraf-init"
 	DefaultTelegrafInitImageTag  = "release-1.5.2"
 	DefaultTelegrafRaidImageName = "telegraf-raid-plugin"
-	DefaultTelegrafRaidImageTag  = "release-1.5.3"
+	DefaultTelegrafRaidImageTag  = "release-1.6.1"
 )
 
 func addDefaultingFuncs(scheme *runtime.Scheme) error {
