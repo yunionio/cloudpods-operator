48114fd5d85fb1e7b953c302f1e47b304b5f2130
Qiu Jian <qiujian@yunionyun.com>
2022-07-16 01:05:31 +0800

    fix: update vendor 20220716

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -49,8 +49,8 @@ build_bin() {
         -v $SRC_DIR:/root/go/src/yunion.io/x/$PROJ \
         -v $SRC_DIR/_output/alpine-build:/root/go/src/yunion.io/x/$PROJ/_output \
         -v $SRC_DIR/_output/alpine-build/_cache:/root/.cache \
-        registry.cn-beijing.aliyuncs.com/yunionio/alpine-build:1.1-5 \
-        /bin/sh -c "set -ex; cd /root/go/src/yunion.io/x/$PROJ;
+        registry.cn-beijing.aliyuncs.com/yunionio/alpine-build:3.16.0-go-1.18.2-0 \
+        /bin/sh -c "set -ex; git config --global --add safe.directory /root/go/src/yunion.io/x/$PROJ; cd /root/go/src/yunion.io/x/$PROJ;
         $BUILD_ARCH $BUILD_CC $BUILD_CGO SHELL='sh -x' GOOS=linux make $component;
         chown -R $(id -u):$(id -g) _output;
         find _output/bin -type f |xargs ls -lah"
