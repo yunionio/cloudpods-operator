81297f7dff6e655349b7cb3ed5a0c72bd743fc5e
Qiu Jian <qiujian@yunionyun.com>
2024-04-04 19:38:49 +0800

    fix: update user password only when -sync-user

--- pkg/util/dbutil/db.go
+++ pkg/util/dbutil/db.go
@@ -4,6 +4,7 @@ type IConnection interface {
 	IsDatabaseExists(db string) (bool, error)
 	IsUserExists(username string) (bool, error)
 	CreateUser(username string, password string, database string) error
+	UpdateUser(username string, password string, database string) error
 	CreateDatabase(db string) error
 	Close() error
 }
